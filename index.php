78338c3e35cb74355ea1d73ec62540dae176b9fd
Greg Bastianelli <gregbast1994@gmail.com>
2025-01-04 11:58:20 -0500

    fix variables and color scheme

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
       while ( have_posts() ) : the_post();
         ?>
         <article>
-          <h2><?php the_title(); ?></h2>
+          <h2><?php //the_title(); ?></h2>
           <div>
-            <?php the_content(); ?>
+            <?php // the_content(); ?>
           </div>
         </article>
         <?php
